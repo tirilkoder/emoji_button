ec3e512a7ada2d7aefe59c15404e7fd51b38741f
tirilkoder <tiril.koder@gmail.com>
2023-10-13 13:07:12 +0200

    Removed function

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ const emojiList = document.querySelector("#emoji_list");
 console.log(emojiList);
 
 const emojis = {
-    catEmojis: ["😺", "😸", "😹", "😻", "😼", "😽", "🙀", "😿", "😾", "🐱"],
-    dogEmojis: ["🐶", "🐕", "🦮", "🐩", "🐾", "🐺", "🦊", "🦝", "🐕‍🦺", "🐩‍🦱"],
+    catEmojis: ["😺", "😸", "😹", "😻", "😼", "😽", "🙀", "😿", "😾", "🐱"]
 };
 
 const list = document.querySelector("#emoji_list")
@@ -14,12 +13,5 @@ function renderEmojis() {
         let newListElement = document.createElement("li");
         newListElement.innerText = emojis.catEmojis[i];
         list.appendChild(newListElement);
-    }
-
-    for (i = 0; i < emojis.dogEmojis.length; i++) {
-      let newListElement = document.createElement("li");
-      newListElement.innerText = emojis.dogEmojis[i];
-      list.appendChild(newListElement);
-  }
-    
+    }   
 }
